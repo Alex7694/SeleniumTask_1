6bd10644560e98c5811b21c27464c0a7b550c587
Alex7694 <ztoropov@gmail.com>
2020-11-21 12:00:44 +0300

    "Задание 2"

---
+++ src/main/java/YandexMarket/util/AllureListener.java
@@ -0,0 +1,20 @@
+package YandexMarket.util;
+
+import SberbankInsuarance.steps.BaseTest;
+import org.junit.runner.notification.Failure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+public class AllureListener extends ru.yandex.qatools.allure.junit.AllureRunListener {
+    @Override
+    public void testFailure(Failure failure) {
+        takeScreenShot();
+        super.testFailure(failure);
+    }
+
+    @Attachment(type = "image/png", value = "Скриншот при ошибке" )
+    public byte[] takeScreenShot() {
+        return ((TakesScreenshot) BaseTest.getDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
